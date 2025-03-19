09e139ddec610dd59968dbdbdc98662a75113e2b
Arie Dallas <ariedallas02@gmail.com>
2025-03-18 21:50:24 -0700

    Make 'JOURNAL' folder if doesn't already exist

--- LUMO_LIBRARY/lumo_journal.py
+++ LUMO_LIBRARY/lumo_journal.py
@@ -1,9 +1,11 @@
 import datetime
 import os
+import pathlib
 import string
 import subprocess
 import time
 
+import LUMO_LIBRARY.lumo_animationlibrary as l_animators
 import LUMO_LIBRARY.lumo_filehandler as l_files
 
 settings = l_files.get_json_settings()
@@ -20,7 +22,12 @@ journal_name = today.strftime(f"%Y_{curr_month_lttr}_%b_%d")
 journal_filename = f"{journal_name}__journal.txt"
 journal_abspath = os.path.join(l_files.journal_folder, journal_filename)
 
+
 def make_journal_file():
+    if not os.path.exists(l_files.journal_folder):
+        l_animators.animate_text("Creating 'JOURNAL' folder", finish_delay=.5)
+        pathlib.Path(l_files.journal_folder).mkdir()
+
     with open(journal_abspath, "w+") as journal:
         journal.write(journal_title_long)
         journal.write("\n\n")
@@ -28,9 +35,17 @@ def make_journal_file():
 
 def get_or_make_journal():
     if os.path.exists(journal_abspath):
-        pass
+        with open(journal_abspath, "r") as journal:
+            lines = journal.readlines()
+
+        with open(journal_abspath, "a") as journal:
+
+            if lines[-1] != ("\n") and len(lines) > 3:
+                journal.write("\n")
+
     else:
         make_journal_file()
+
 
 def program_header():
     print("JOURNAL")
@@ -39,7 +54,7 @@ def program_header():
 
 
 def main():
-    program_header()
+    # program_header()
     get_or_make_journal()
     subprocess.run([f"{settings.get("text editor")} +9999999 {journal_abspath}"], shell=True)
 
