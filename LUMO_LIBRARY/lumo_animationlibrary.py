3ec7e4d164163b858cb35779d0fdba2b05e845cb
Arie Dallas <ariedallas02@gmail.com>
2025-04-09 15:29:40 -0700

    Add type hints; clean up file.

--- LUMO_LIBRARY/lumo_animationlibrary.py
+++ LUMO_LIBRARY/lumo_animationlibrary.py
@@ -1,13 +1,8 @@
 import time
+from typing import Optional
 
 
-def animate_pause(no_of_lines: int, speed: float = .5):
-    for x in range(no_of_lines):
-        print()
-        time.sleep(speed)
-
-
-def animate_text(text, speed: float = .025, finish_delay: float = 0):
+def animate_text(text: str, speed: float =.025, finish_delay: float=0) -> None:
     text_add_space = " " + text
     for n in range(len(text_add_space)):
         print(text_add_space[:n], end="\r")
@@ -21,11 +16,11 @@ def animate_text(text, speed: float = .025, finish_delay: float = 0):
         time.sleep(finish_delay)
 
 
-def animate_text_indented(text, speed: float = .025, indent=None, finish_delay: float = 0):
+def animate_text_indented(text: str, speed: float=.025, indent=Optional[int], finish_delay: float=0) -> None:
     if not indent:
         indent = 0
 
-    text_add_space = (" " * int(indent)) + text
+    text_add_space = (" " * indent) + text
 
     for n in range(indent, len(text_add_space) + 1):
         print(text_add_space[:n], end="\r")
@@ -39,19 +34,10 @@ def animate_text_indented(text, speed: float = .025, indent=None, finish_delay: 
         time.sleep(finish_delay)
 
 
-def animate_text_fast(text):
-    text_add_space = " " + text
-    for n in range(len(text_add_space)):
-        print(text_add_space[:n], end="\r")
-        time.sleep(.02)
+def list_printer(text_list: Optional[list[str]] = None,
+                 indent_amt: int=0,
+                 speed_interval: float=.35) -> None:
 
-    print(text, end="\n")
-    time.sleep(.5)
-
-
-def list_printer(text_list: list[str] | None = None,
-                 indent_amt: int = 0,
-                 speed_interval: float = .35):
     if not text_list:
         text_list = [""]
 
@@ -67,9 +53,5 @@ def list_printer(text_list: list[str] | None = None,
 
 
 if __name__ == "__main__":
-    list_printer()
+    animate_text_indented("boooo!", indent=2)
 
-
-def fixed_interval_delay(text: str, interval: float = .35):
-    animate_text(text)
-    time.sleep(interval)
