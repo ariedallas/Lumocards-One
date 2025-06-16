7511f95824d873f50fe25eeae3d16d67047c2bd4
Arie Dallas <ariedallas02@gmail.com>
2025-06-16 15:19:06 -0700

    Update pyinstaller scripts for linux/mac.

---
+++ pyinstaller_mac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ pyinstaller LUMO_LIBRARY/__main__.py\
+ 	--name Lumogarden\
+	--onedir\
+	--windowed\
+	--distpath Lumogarden_Project\
+	--add-data ./__USER_FILES__:./__USER_FILES__\
+	--add-data ./__SUPPORT_FILES__:./__SUPPORT_FILES__\
+	--add-binary micro:.\
+
