cefa024f0511d54f7496c74a753dc1c44d558a33
Arie Dallas <ariedallas02@gmail.com>
2025-06-28 10:56:16 -0700

    Remove installation files scripts.

--- pyinstaller_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
- pyinstaller LUMO_LIBRARY/__main__.py\
- 	--name Lumogarden\
-	--onedir\
-	--distpath Lumogarden_Project\
-	--add-data ./__USER_FILES__:./__USER_FILES__\
-	--add-data ./__SUPPORT_FILES__:./__SUPPORT_FILES__\
-	--add-binary micro:.\
